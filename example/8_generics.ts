bc164f8b681392ab19654365aa06cd71df07f577
chanyu1 <haneul1ij@gmail.com>
2022-03-03 23:47:54 +0900

    learn TS

--- example/8_generics.ts
+++ example/8_generics.ts
@@ -6,11 +6,17 @@ function getArray(value: string[]) {
   return value;
 }
 
+function logText(text: string | number) {
+  return text;
+}
+// error
+// logText("a").split("");
+
 // 제네릭 기본 문법 - 함수
 function getValue<T>(value: T): T {
   return value;
 }
-getValue('hi').toLocaleUpperCase();
+getValue("hi").toLocaleUpperCase();
 getValue(100).toLocaleString();
 
 // 제네릭 기본 문법 - 인터페이스
@@ -18,7 +24,7 @@ interface Developer<T> {
   name: string;
   age: T;
 }
-const tony: Developer<number> = { name: 'tony', age: 100 };
+const tony: Developer<number> = { name: "tony", age: 100 };
 
 // 제네릭 타입 제한 - 구체적인 타입
 function getNumberAndArray<T>(value: T): T {
@@ -39,15 +45,15 @@ interface ShoppingItems {
   stock: number;
 }
 function getAllowedOptions<T extends keyof ShoppingItems>(option: T): T {
-  if (option === 'name' || option === 'address') {
-    console.log('option type is string');
+  if (option === "name" || option === "address") {
+    console.log("option type is string");
     return option;
   }
-  if (option === 'price' || option === 'stock') {
-    console.log('option type is number');
+  if (option === "price" || option === "stock") {
+    console.log("option type is number");
     return option;
   }
 }
-getAllowedOptions('nothing');
+getAllowedOptions("nothing");
 // const a = getAllowedOptions('name');
 // a.toUpperCase(); // Name
